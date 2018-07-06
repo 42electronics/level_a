d81f888d9c0f0b1cc94acfdc9d37d8e24ade6a36
42 Electronics <support@42electronics.com>
2018-07-06 18:03:00 +0000

    Initial commit

---
+++ lesson_15/Activity2_Current_Age.py
@@ -0,0 +1,32 @@
+#Copyright (c) 2018 42 Development dba 42 Electronics
+#Author: Eric Feickert
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy of
+#this software and associated documentation files (the "Software"), to deal in the
+#Software without restriction, including without limitation the rights to use,
+#copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+#Software, and to permit persons to whom the Software is furnished to do so,
+#subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+#INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+#PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+#HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+#OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+#SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+year = input('What year were you born: ')
+bday = input('Has your birthday happened this year (y/n): ')
+
+year_int = int(year)
+age = 2018 - year_int
+
+if bday == 'n':
+    age = age - 1
+
+age_str = str(age)
+
+print('Your current age is ' + age_str)
---
+++ lesson_18/Activity2_Reaction_Game.py
@@ -0,0 +1,51 @@
+#Copyright (c) 2018 42 Development dba 42 Electronics
+#Author: Eric Feickert
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy of
+#this software and associated documentation files (the "Software"), to deal in the
+#Software without restriction, including without limitation the rights to use,
+#copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+#Software, and to permit persons to whom the Software is furnished to do so,
+#subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+#INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+#PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+#HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+#OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+#SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import RPi.GPIO as GPIO
+import time
+import random
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(26, GPIO.OUT)
+GPIO.setup(20, GPIO.IN)
+GPIO.setup(21, GPIO.IN)
+
+wait = random.randint(1,5)
+time.sleep(wait)
+
+GPIO.output(26,GPIO.HIGH)
+timestart = time.time()
+
+winner = 'No'
+
+while winner == 'No':
+    if GPIO.input(20) == True:
+        winner = 'Player 1'
+    if GPIO.input(21) == True:
+        winner = 'Player 2'
+
+time = time.time() - timestart
+time = "%.3f" % time
+
+print(winner + ' wins!')
+print('Your reaction time was ' + time + ' seconds')
+
+GPIO.output(26, GPIO.LOW)
+GPIO.cleanup()
